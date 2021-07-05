9825fc11b93eb8addc625c621fa51bb3b020866e
Robin Grabs <robinmgrabs@gmail.com>
2021-07-05 17:29:08 +0200

    Add project files.

---
+++ StringManipulationExercise/StringManipulationService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StringManipulationExercise
+{
+    public class StringManipulationService
+    {
+        public string ReverseString(string input)
+        {
+            return input;
+        }
+
+        public string EvenOdd(string input)
+        {
+            return input;
+        }
+    }
+}
